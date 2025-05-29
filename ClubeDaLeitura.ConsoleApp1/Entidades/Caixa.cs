708994a76c4a89931fe265ac8031b51602e48104
GabrielBGB <gabrielprogramacao@outlook.com.br>
2025-05-29 11:11:41 -0300

    Implementado a classe Caixa.cs

---
+++ ClubeDaLeitura.ConsoleApp1/Entidades/Caixa.cs
@@ -0,0 +1,28 @@
+﻿namespace ClubeDaLeitura.Entidades
+{
+    public class Caixa
+    {
+        public int Id { get; set; }
+        public string Etiqueta { get; set; }
+        public string Cor { get; set; }
+        public int DiasEmprestimo { get; set; }
+
+        public List<int> IdsRevistas { get; set; } = new();
+
+        public bool Validar(out string erros)
+        {
+            erros = "";
+
+            if (string.IsNullOrWhiteSpace(Etiqueta) || Etiqueta.Length > 50)
+                erros += "Etiqueta é obrigatória e deve ter no máximo 50 caracteres.\n";
+
+            if (string.IsNullOrWhiteSpace(Cor))
+                erros += "Cor é obrigatória.\n";
+
+            if (DiasEmprestimo <= 0)
+                erros += "Dias de empréstimo deve ser maior que 0.\n";
+
+            return erros == "";
+        }
+    }
+}
