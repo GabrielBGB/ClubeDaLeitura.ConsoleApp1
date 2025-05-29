e800d410771bdd629bf5a2dca2cd5bc4e4f6c828
GabrielBGB <gabrielprogramacao@outlook.com.br>
2025-05-29 11:24:44 -0300

    Implementado a classe RepositorioCaixa.cs e feiro as correções nos bug do codigo

---
+++ ClubeDaLeitura.ConsoleApp1/Repositorios/RepositorioCaixa.cs
@@ -0,0 +1,47 @@
+﻿using ClubeDaLeitura.Entidades;
+
+namespace ClubeDaLeitura.Repositorios
+{
+    public class RepositorioCaixa
+    {
+        private List<Caixa> caixas = new();
+        private int contadorId = 1;
+
+        public void Inserir(Caixa caixa)
+        {
+            caixa.Id = contadorId++;
+            caixas.Add(caixa);
+        }
+
+        public bool Editar(int id, Caixa nova)
+        {
+            var caixa = SelecionarPorId(id);
+            if (caixa == null) return false;
+
+            caixa.Etiqueta = nova.Etiqueta;
+            caixa.Cor = nova.Cor;
+            caixa.DiasEmprestimo = nova.DiasEmprestimo;
+
+            return true;
+        }
+
+        public bool Excluir(int id)
+        {
+            var caixa = SelecionarPorId(id);
+            if (caixa == null || caixa.IdsRevistas.Any())
+                return false;
+
+            caixas.Remove(caixa);
+            return true;
+        }
+
+        public List<Caixa> SelecionarTodos() => caixas;
+
+        public Caixa? SelecionarPorId(int id) => caixas.FirstOrDefault(c => c.Id == id);
+
+        public bool ExisteEtiqueta(string etiqueta)
+        {
+            return caixas.Any(c => c.Etiqueta.Equals(etiqueta, StringComparison.OrdinalIgnoreCase));
+        }
+    }
+}
